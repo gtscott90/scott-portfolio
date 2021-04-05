b0b141eb581a63ee5b874a2df31ca169f08ca243
Greg Scott <gtscott90@gmail.com>
2021-04-04 22:13:50 -0400

    wrote navbar logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Greg Scott
+        </Link>
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={
+                  window.location.pathname === "/" || window.location.pathname === "/about"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/portfolio"
+                className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
