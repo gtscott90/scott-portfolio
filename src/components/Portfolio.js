b9b3ddfa3852b2927b44ae3ddfe05f61f70a453c
Greg Scott <gtscott90@gmail.com>
2021-04-04 22:50:31 -0400

    placeholder for Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+function Portfolio() {
+    return (
+      <div>
+          <span>Placeholder for Portfolio</span>
+      </div>
+    );
+  }
+  
+  export default Portfolio;
