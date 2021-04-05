4d605cc9bc7f99ad5f86bad9f445d42273297c81
Greg Scott <gtscott90@gmail.com>
2021-04-04 22:42:35 -0400

    placeholder for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <span>footer placeholder</span>
+      </footer>
+    );
+  }
+  
+  export default Footer;
