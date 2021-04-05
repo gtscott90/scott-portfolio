48e3f623a4d3e9e3680bb4758617915c08612753
Greg Scott <gtscott90@gmail.com>
2021-04-04 22:48:53 -0400

    placeholder for Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+function Contact() {
+    return (
+      <div>
+          <span>Placeholder for Contact</span>
+      </div>
+    );
+  }
+  
+  export default Contact;
