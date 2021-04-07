6dafdccec351f2e01220533f82596414c36474ca
Greg Scott <gtscott90@gmail.com>
2021-04-06 20:37:32 -0400

    wrote the footer code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 function Footer() {
     return (
       <footer className="footer">
-        <span>footer placeholder</span>
+        <div className="listedContact">
+                    <a href="https://www.linkedin.com/in/greg-scott-4a94b783/"><i className="fab fa-linkedin-in"> Linked In</i></a> &emsp; <a href="https://github.com/gtscott90"><i className="fab fa-github"> Github Profile</i></a>
+                    <p>gtscott90@gmail.com &emsp; (937) 974-2843</p>
+                    <a href="../../public/files/Scott_Gregory_Resume.pdf" download>Resume</a>
+                </div>
       </footer>
     );
   }
