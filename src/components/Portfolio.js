21fbcdd73370afe224ac33a26f38906005ff07c8
Greg Scott <gtscott90@gmail.com>
2021-06-07 11:14:09 -0400

    added DevSync to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,7 @@ import DayPlanner from "./images/dayplanner.png"
 import PassGen from "./images/passwordGenerator.png"
 import DestinationGoals from "./images/destination-goals.png"
 import EatBurger from "./images/eat-da-burger.png"
+import DevSync from "./images/devsync.png"
 
 function Portfolio() {
     return (
@@ -18,27 +19,27 @@ function Portfolio() {
             <hr />
            <div className="row">
                 <div className="col-lg-3">
+                    <figure className="text-center"><img src={DevSync} alt="DevSync" style={{width: "150px"}}/> 
+                        <figcaption className="text-center"><a href="https://devsync-project-3.herokuapp.com/">DevSync</a></figcaption>
+                        <figcaption className="text-center"><a href="https://github.com/gtscott90/Project-3">Github Repository</a></figcaption>
+                    </figure>
+                </div>
+                <div className="col-lg-3">
                     <figure className="text-center"><img src={Spyglass} alt="Spyglass" style={{width: "150px"}}/> 
                         <figcaption className="text-center"><a href="https://gabel35.github.io/Spyglass/">Spyglass</a></figcaption>
                         <figcaption className="text-center"><a href="https://github.com/gabel35/Spyglass">Github Repository</a></figcaption>
                     </figure>
                 </div>
                 <div className="col-lg-3">
-                    <figure className="text-center"><img src={DayPlanner} alt="Day Planner Icon" style={{width: "150px"}}/> 
-                        <figcaption className="text-center"><a href="https://gtscott90.github.io/day-planner/">Day Planner</a></figcaption>
-                        <figcaption className="text-center"><a href="https://github.com/gtscott90/day-planner">Github Repository</a></figcaption>
-                    </figure>
-                </div>
-                <div className="col-lg-3">
-                    <figure className="text-center"><img src={PassGen} alt="Password Generator" style={{width: "150px"}}/> 
-                        <figcaption className="text-center"><a href="https://gtscott90.github.io/password-generator/">Password Generator</a></figcaption>
-                        <figcaption className="text-center"><a href="https://github.com/gtscott90/password-generator">Github Repository</a></figcaption>
-                    </figure>
-                </div>
-                <div className="col-lg-3">
                     <figure className="text-center"><img src={DestinationGoals} alt="Destination: Goals" style={{width: "150px"}}/> 
                         <figcaption className="text-center"><a href="https://apple-tart-97413.herokuapp.com/">Destination: Goals</a></figcaption>
                         <figcaption className="text-center"><a href="https://github.com/jacquelinerodney/project2">Github Repository</a></figcaption>
+                    </figure>
+                </div>
+                <div className="col-lg-3">
+                    <figure className="text-center"><img src={DayPlanner} alt="Day Planner Icon" style={{width: "150px"}}/> 
+                        <figcaption className="text-center"><a href="https://gtscott90.github.io/day-planner/">Day Planner</a></figcaption>
+                        <figcaption className="text-center"><a href="https://github.com/gtscott90/day-planner">Github Repository</a></figcaption>
                     </figure>
                 </div>
            </div>
@@ -48,6 +49,12 @@ function Portfolio() {
                     <figure className="text-center"><img src={EatBurger} alt="Eat Da Burger" style={{width: "150px"}}/> 
                         <figcaption className="text-center"><a href="https://fast-thicket-43734.herokuapp.com/">Eat Da Burger!</a></figcaption>
                         <figcaption className="text-center"><a href="https://github.com/gtscott90/burger-eater">Github Repository</a></figcaption>
+                    </figure>
+                </div>
+                <div className="col-lg-3">
+                    <figure className="text-center"><img src={PassGen} alt="Password Generator" style={{width: "150px"}}/> 
+                        <figcaption className="text-center"><a href="https://gtscott90.github.io/password-generator/">Password Generator</a></figcaption>
+                        <figcaption className="text-center"><a href="https://github.com/gtscott90/password-generator">Github Repository</a></figcaption>
                     </figure>
                 </div>
             </div>
