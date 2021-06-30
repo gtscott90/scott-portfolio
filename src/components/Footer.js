1520729e35f13760c3edb91af80d20b9812926ac
Greg Scott <gtscott90@gmail.com>
2021-06-30 17:47:17 -0400

    adjusted file path for resume download

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div className="listedContact">
                     <a href="https://www.linkedin.com/in/greg-scott-4a94b783/"><i className="fab fa-linkedin-in"> Linked In</i></a> &emsp; <a href="https://github.com/gtscott90"><i className="fab fa-github"> Github Profile</i></a>
                     <p>gtscott90@gmail.com &emsp; (937) 974-2843</p>
-                    <a href="files/Scott_Gregory_Full Stack Resume.pdf" download>Resume</a>
+                    <a href="Scott_Gregory_Full Stack Resume.pdf" download>Resume</a>
                 </div>
       </footer>
     );
