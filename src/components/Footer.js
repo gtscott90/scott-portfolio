ac2527c9b27138ffc70a6e7604e8825fa102f371
Greg Scott <gtscott90@gmail.com>
2021-08-10 15:37:47 -0400

    Footer: linkedin and github now have fontawesome icons and open in new tabs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
 // import { BrowserRouter as Router } from "react-router-dom";
 import "./style.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const falinkedIn = <FontAwesomeIcon icon={faLinkedin} />
+const faGitHub = <FontAwesomeIcon icon={faGithub} />
 
 function Footer() {
     return (
     <footer className="footer">
           <div className="listedContact">
-                      <a href="https://www.linkedin.com/in/gregscottdev/"><i className="fab fa-linkedin-in"> Linked In</i></a> &emsp; <a href="https://github.com/gtscott90"><i className="fab fa-github"> Github Profile</i></a>
+                      <a href="https://www.linkedin.com/in/gregscottdev/" target="_blank">{falinkedIn} Linked In</a> &emsp; <a href="https://github.com/gtscott90" target="_blank">{faGitHub} Github Profile</a>
                       <p>gtscott90@gmail.com &emsp; (937) 974-2843</p>
                       
                   </div>
