2e274279667cc3b3b042afa5730d5364078342b9
Greg Scott <gtscott90@gmail.com>
2021-08-10 21:11:34 -0400

    put resume file in file sharing direct download link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
           <div className="listedContact">
                       <a href="https://www.linkedin.com/in/gregscottdev/" target="_blank">{falinkedIn} Linked In</a> &emsp; <a href="https://github.com/gtscott90" target="_blank">{faGitHub} Github Profile</a>
                       <p>gtscott90@gmail.com &emsp; (937) 974-2843</p>
-                      
+                      <a href="https://dl.dropboxusercontent.com/s/cs7mq5csnekul3o/Scott_Gregory_Resume.pdf?dl=0" target="_blank" download>Resume</a>
                   </div>
         </footer>
     );
@@ -23,10 +23,4 @@ function Footer() {
   
   export default Footer;
 
-  // <footer className="footer">
-  //       <div className="listedContact">
-  //                   <a href="https://www.linkedin.com/in/greg-scott-4a94b783/"><i className="fab fa-linkedin-in"> Linked In</i></a> &emsp; <a href="https://github.com/gtscott90"><i className="fab fa-github"> Github Profile</i></a>
-  //                   <p>gtscott90@gmail.com &emsp; (937) 974-2843</p>
-  //                   <Link to="/files/Scott_Gregory_Full Stack Resume.pdf" target="_blank" download>Resume</Link>
-  //               </div>
-  //     </footer>
+ 
